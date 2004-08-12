9d245f96db7a20606bcc18c85477f5e84dae81e5
Ivan Tubert-Brohman <ivan.tubert@gmail.com>
2004-08-12 20:37:09 +0000

    Added the website to CVS

---
+++ mypod2html.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+# mypod2html.pl [options] files...
+# options: 
+# -d <depth>   delete the first <depth> path levels 
+# -s <n>       use menu section <n> for the template
+# -b <base>    root path to create directories
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Std;
+use lib '/home/ivan/devel/Pod-HtmlEasy';
+use Pod::HtmlEasy;
+
+my %opts;
+getopts("d:s:b:", \%opts);
+
+my $podhtml = Pod::HtmlEasy->new(
+    on_L  => sub {
+        my ( $this , $L , $text, $page , $section, $type ) = @_ ;
+        if ( $type eq 'pod' ) {
+            $section = defined $section ? "#$section" : '';
+            return "<i><a href='/cgi-bin/perldoc?$page$section'>$text</a></i>" ;
+        } elsif( $type eq 'man' ) { 
+            return "<i>$text</i>" ;
+        } elsif( $type eq 'url' ) { 
+            return "<a href='$page'>$text</a>" ;
+        }
+      } ,
+);
+
+
+for my $file (@ARGV) {
+
+    my $name = $podhtml->pm_name($file);
+
+    my $section = $opts{s} ? "section=$opts{s}" : "";
+    my $base    = $opts{b} || '.';
+    my $head = <<TT;
+[% PROCESS head title="$name" %]
+[% INCLUDE nav.html $section %]
+<div class="main">
+TT
+    my $foot = "</div></body></html>\n";
+
+    my @path = split '/', $file;
+
+    splice @path, 0, $opts{d} || 0;
+    my $path = join "/", @path;
+
+    $path =~ s/\.(pm|pod)//;
+    $path .= ".html";
+    my ($dir) = $path =~ m|^(.*)/|;
+    $dir ||= '';
+    $dir = "$base/$dir";
+    print "$path\n";
+    mkpath($dir);
+
+    my $html = $podhtml->pod2html( $file, undef,
+        body => '', 
+        title => "PerlMol Doc - $name",
+        no_index => 1,
+        only_content => 1,
+
+    );
+
+    #print "$base/$path";
+    open F, ">$base/$path" or die;
+    print F $head, $html, $foot;
+    close F;
+}
---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "converting PODs to HTML"
+./mypod2html.pl -d6 -b tt/pod `cat podlist.txt`
+./mypod2html.pl -d6 -b tt/pod -s3 `cat tut.txt`
+./mypod2html.pl -d3 -b tt/pod `cat bins.txt`
+./mypod2html.pl -d7 -b tt/pod `cat index.txt`
+
+echo "converting PODs to PDF"
+cd tex
+pod2latex -full -modify -out perlmol.tex `cat pods2.txt`
+pslatex perlmol.tex
+pslatex perlmol.tex
+dvipdf perlmol.dvi
+cd ..
+cp tex/perlmol.pdf tt/pod/perlmol.pdf
+
+echo "running templates"
+ttree -f tt.conf
+
+echo "creating tarball"
+tar czf web.tgz htdocs
+echo "copying tarball"
+scp web.tgz shell.sf.net:perlmol
+echo "installing tarball"
+ssh shell.sf.net 'cd perlmol && tar xvzf web.tgz'
+echo installing CGIs
+scp cgi/* shell.sf.net:perlmol/cgi-bin
+
+echo "done"
